b43cd2642443e7a4bbc7a772b242182b3574f4af
Vitor Studzieski <vitorcorreastudzieski@gmail.com>
2026-09-11 09:36:30 -0300

    fix: atualiza cache dos scripts do acompanhamento

--- app/acompanhar/[token]/page.jsx
+++ app/acompanhar/[token]/page.jsx
@@ -1,7 +1,7 @@
 import Script from "next/script";
 import HtmlTemplate from "../../components/shared/HtmlTemplate";
 
-const TRACKING_ASSET_VERSION = "2026.09.11.6";
+const TRACKING_ASSET_VERSION = "2026.09.11.7";
 
 export const metadata = {
   title: "Acompanhar senha",
