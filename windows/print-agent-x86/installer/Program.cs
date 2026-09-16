d044bbc7bf75b4f946da29f9b95f2e839c1f16af
Vitor Studzieski <vitorcorreastudzieski@gmail.com>
2026-09-16 11:15:20 -0300

    fix: include installer thread exception type

--- windows/print-agent-x86/installer/Program.cs
+++ windows/print-agent-x86/installer/Program.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Reflection;
 using System.ServiceProcess;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace SenhaHub.PrintAgent.Setup
